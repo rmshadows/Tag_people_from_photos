cc968551fc7e4c2d1196e123d1b18bdd3e2285f6
Ryan <justaaaa@163.com>
2020-06-27 22:47:07 +0800

    update RESET_FRS.sh.

--- RESET_FRS.sh
+++ RESET_FRS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -r ./FR_DATA
+mkdir ./FR_DATA
 mkdir ./FR_DATA/A-KnownPeople ./FR_DATA/B-Unknown ./FR_DATA/C-Noneface ./FR_DATA/D-Singleface ./FR_DATA/E-Morefaces
 rm -r ./INPUT_PIC/*
 rm -r ./Prescreen/*
