473bfd48483d3c72cc031af10ab5060880d37420
Ryan <justaaaa@163.com>
2020-06-28 10:26:23 +0800

    update eaCLEAN_UP_TEMP.py.

--- eaCLEAN_UP_TEMP.py
+++ eaCLEAN_UP_TEMP.py
@@ -23,6 +23,7 @@ def cleanUpTemp():
 		commandImplementation = os.popen(commandInput)
 	except Exception as e:
 		print("ERROR 2")
+	print("\n\033[5;31;40m临时文件已经清除。下面如有报错，请忽略。\033[0m\n")
 
 if __name__ == "__main__":
 	cleanUpTemp()
