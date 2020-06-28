29c0783531b2ba9892fa2dacdc65c0708ab6d296
Ryan <justaaaa@163.com>
2020-06-28 10:27:15 +0800

    update fbTrainingDemo.py.

--- fbTrainingDemo.py
+++ fbTrainingDemo.py
@@ -1,31 +1,47 @@
-# -*- coding: utf-8 -*-
+import os
+from datetime import datetime
 '''
-演示前的工作：
-添加要训练的材料到 Prescreen文件夹中：
-结构：
-    -+-PersonA-+-1.jpg
-     |         +-2.jpg
-     |         +-...
-     |
-     +-PersonB-+-1.jpg
-     |         +-2.jpg
-     |         +-...
-     |
-     +-PersonC-+-1.jpg
-               +-2.jpg
-               +-...
+从已识别的数据库中搜寻人物，结果会创建一个检索词开头的文件夹。
+'''
 
-'''
-import aaPrescreenPicture
-import abAddKnownPerson
-import baTrainingOneProcessing
+def findSingleFaceData(who,time):
+	person=os.listdir("./FR_DATA/D-Singleface/")
+	found=False
+	dst="./{0}{1}".format(who,time)
+	os.makedirs(dst)
+	for unit in person:
+		if who in unit:
+			found=True
+			try:
+				commandInput = "cp -r ./FR_DATA/D-Singleface/{0} {1}".format(unit,dst)
+				commandImplementation = os.popen(commandInput)
+			except Exception as e:
+				print("MOVE FILE ERROR.")
+		else:
+			pass
+	if found:
+		print("在单人面孔中发现{}。".format(who))
+	else:
+		print("未发现{}的单人面孔数据。".format(who))
+	return dst
+
+def findMultiFacesData(who,time,dst):
+	person=os.listdir("./FR_DATA/E-Morefaces/")
+	for unit in person:
+		if who in unit:
+			try:
+				commandInput = "cp ./FR_DATA/E-Morefaces/{0} {1}".format(unit,dst)
+				commandImplementation = os.popen(commandInput)
+			except Exception as e:
+				print("MOVE FILE ERROR.")
+
+def FindSomebody(name):
+	print("\033[5;32;40m新建检索结果文件夹...\033[0m")
+	time=str(datetime.now())
+	dst=findSingleFaceData(name,time[11:])
+	#print(dst)
+	findMultiFacesData(name,time,dst)
+	print("\n\033[5;31;40m检索完毕\033[0m\n")
 
 if __name__ == "__main__":
-	print("处理训练材料。")
-	aaPrescreenPicture.filePrescreen()
-	print("添加训练素材。")
-	abAddKnownPerson.addTrainLib()
-	print("开始训练。")
-	#训练文件夹（FR_DATA中）和训练模型名称（在KNN_MOD中）
-	baTrainingOneProcessing.main("A-KnownPeople","KnowPeopleDemo")
-	print("训练完毕。")
+	FindSomebody("Albert")#要查找的人名
