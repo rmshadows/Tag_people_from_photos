d7139c1e39be21592b010766265e5536b2d8ea32
Ryan <justaaaa@163.com>
2020-06-29 10:58:34 +0800

    update abAddKnownPerson.py.

--- abAddKnownPerson.py
+++ abAddKnownPerson.py
@@ -4,23 +4,50 @@ import os
 '''
 把处理过的、经过人工审核的已知人物移动到人像库(./FR_DATA/A-KnownPeople/)中
 '''
-person = os.listdir("./Prescreen")
-for x in person:
+
+def addPerson(name):
 	try:
-		com="mkdir ./D-Singleface/{0}".format(x)
+		commandInput = "mv ./Prescreen/{0} ./FR_DATA/A-KnownPeople/".format(name)
+		commandImplementation = os.popen(commandInput)
+		print("Person created.")
+	except Exception as e:
+		raise e
+
+def addPicture(name):
+	try:
+		commandInput = "mv ./Prescreen/{0}/* ./FR_DATA/A-KnownPeople/{1}/".format(name,name)
+		commandImplementation = os.popen(commandInput)
+		print("Person existed,adding....")
+	except Exception as e:
+		raise e
+
+def addSingleDir(name):
+	try:
+		com="mkdir ./FR_DATA/D-Singleface/{0}".format(name)
 		mkdir = os.popen(com)
 	except Exception as e:
 		raise e
 
-def addTrainLib():
+def addKnowPeople():
+	add = os.listdir("./Prescreen")
+	for name in add:
+		if not os.path.exists("./FR_DATA/A-KnownPeople/{0}".format(name)):
+			addPerson(name)
+		else:
+			addPicture(name)
+		if not os.path.exists("./FR_DATA/D-Singleface/{0}".format(name)):
+			addSingleDir(name)
+		else:
+			pass
+	print("人物添加完毕。")
 	try:
-		commandInput = 'mv ./Prescreen/* ./FR_DATA/A-KnownPeople/'
+		commandInput = "rm -r ./Prescreen/*"
 		commandImplementation = os.popen(commandInput)
-		print("File Moved")
+		print("Remove Prescreen....")
 	except Exception as e:
-		print("File Moving failed.")
 		raise e
 
+
 if __name__ == "__main__":
-	addTrainLib()
+	addKnowPeople()
 	print("\033[5;31;40m训练材料移动工作结束，接下来请进行模型训练。下面如果有报错，请忽略。\033[0m\n")
