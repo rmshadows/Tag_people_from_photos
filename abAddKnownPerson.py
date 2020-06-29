8328766d9a6191537c9a4291d9dbe3f0744395a1
Ryan <justaaaa@163.com>
2020-06-29 22:09:19 +0800

    update abAddKnownPerson.py.

--- abAddKnownPerson.py
+++ abAddKnownPerson.py
@@ -5,48 +5,57 @@ import os
 把处理过的、经过人工审核的已知人物移动到人像库(./FR_DATA/A-KnownPeople/)中
 '''
 
-def addPerson(name):
+#新建人物
+def __addPerson(name):
 	try:
+		#mv ./Prescreen/{name} ./FR_DATA/A-KnownPeople/
 		commandInput = "mv ./Prescreen/{0} ./FR_DATA/A-KnownPeople/".format(name)
 		commandImplementation = os.popen(commandInput)
 		print("Person created.")
 	except Exception as e:
 		raise e
 
-def addPicture(name):
+#添加到已有
+def __addPicture(name):
 	try:
+		#mv ./Prescreen/{name}/* ./FR_DATA/A-KnownPeople/{name}/
 		commandInput = "mv ./Prescreen/{0}/* ./FR_DATA/A-KnownPeople/{1}/".format(name,name)
 		commandImplementation = os.popen(commandInput)
 		print("Person existed,adding....")
 	except Exception as e:
 		raise e
 
-def addSingleDir(name):
+#单人目录添加
+def __addSingleDir(name):
 	try:
+		#mkdir ./FR_DATA/D-Singleface/{name}
 		com="mkdir ./FR_DATA/D-Singleface/{0}".format(name)
 		mkdir = os.popen(com)
 	except Exception as e:
 		raise e
 
+#主要方法X
 def addKnowPeople():
-	add = os.listdir("./Prescreen")
+	add = os.listdir("./Prescreen")#./Prescreen/*
 	for name in add:
+		#./FR_DATA/A-KnownPeople/{name}
 		if not os.path.exists("./FR_DATA/A-KnownPeople/{0}".format(name)):
-			addPerson(name)
+			__addPerson(name)
 		else:
-			addPicture(name)
+			__addPicture(name)
+		#./FR_DATA/D-Singleface/{name}
 		if not os.path.exists("./FR_DATA/D-Singleface/{0}".format(name)):
-			addSingleDir(name)
+			__addSingleDir(name)
 		else:
 			pass
 	print("人物添加完毕。")
 	try:
+		#rm -r ./Prescreen/*
 		commandInput = "rm -r ./Prescreen/*"
 		commandImplementation = os.popen(commandInput)
 		print("Remove Prescreen....")
 	except Exception as e:
 		raise e
-
 
 if __name__ == "__main__":
 	addKnowPeople()
