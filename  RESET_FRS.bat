747dabc24689fd2a8f0f54af77dd2c153bc7eaaf
Ryan <justaaaa@163.com>
2020-07-01 16:51:01 +0800

    add RESET_FRS.bat.

---
+++  RESET_FRS.bat
@@ -0,0 +1,19 @@
+rd /S /Q .\FR_DATA
+rd /S /Q .\INPUT_PIC
+rd /S /Q .\Prescreen
+rd /S /Q .\tempMore
+rd /S /Q .\tempNone
+rd /S /Q .\tempSingle
+rd /S /Q .\tempFaceRecognition
+mkdir .\FR_DATA
+mkdir .\FR_DATA\A-KnownPeople
+mkdir .\FR_DATA\B-Unknown
+mkdir .\FR_DATA\C-Noneface
+mkdir .\FR_DATA\D-Singleface
+mkdir .\FR_DATA\E-Morefaces
+mkdir .\INPUT_PIC
+mkdir .\Prescreen
+mkdir .\tempMore
+mkdir .\tempNone
+mkdir .\tempSingle
+mkdir .\tempFaceRecognition
