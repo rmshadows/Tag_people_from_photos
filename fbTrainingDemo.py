86ff3230788bb112f29b9c7407f51ff76e5552e8
Ryan <justaaaa@163.com>
2020-07-01 18:15:46 +0800

    update fbTrainingDemo.py.

--- fbTrainingDemo.py
+++ fbTrainingDemo.py
@@ -3,28 +3,31 @@
 演示前的工作：
 添加要训练的材料到 Prescreen文件夹中：
 结构：
-    -+-PersonA-+-1.jpg
-     |         +-2.jpg
-     |         +-...
-     |
-     +-PersonB-+-1.jpg
-     |         +-2.jpg
-     |         +-...
-     |
-     +-PersonC-+-1.jpg
-               +-2.jpg
-               +-...
+	-+-PersonA-+-1.jpg
+	 |		 +-2.jpg
+	 |		 +-...
+	 |
+	 +-PersonB-+-1.jpg
+	 |		 +-2.jpg
+	 |		 +-...
+	 |
+	 +-PersonC-+-1.jpg
+			   +-2.jpg
+			   +-...
 
 '''
 import aaPrescreenPicture
 import abAddKnownPerson
 import baTrainingOneProcessing
+import Wait
 
 if __name__ == "__main__":
 	print("处理训练材料。")
 	aaPrescreenPicture.filePrescreen()
+	Wait.waiting(2.5)
 	print("添加训练素材。")
-	abAddKnownPerson.addKnowPeople()
+	abAddKnownPerson.addTrainLib()
+	Wait.waiting(2.5)
 	print("开始训练。")
 	#训练文件夹（FR_DATA中）和训练模型名称（在KNN_MOD中）
 	baTrainingOneProcessing.main("A-KnownPeople","KnowPeopleDemo")
