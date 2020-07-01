8d113d08826ca91c50a6e2ada8fb5c82458e6f84
Ryan <justaaaa@163.com>
2020-07-01 18:14:53 +0800

    update ebCLEAN_UP_FRed.py.

--- ebCLEAN_UP_FRed.py
+++ ebCLEAN_UP_FRed.py
@@ -4,32 +4,62 @@
 '''
 import os
 
+WINDOWS=os.sep=="\\"
+SS=os.sep
+
 #mainX
 def cleanUpRecognized():
-	try:
-		commandInput = 'rm ./FR_DATA/B-Unknown/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 0")
+	if WINDOWS:
+		try:
+			commandInput = 'del /S /Q .\\FR_DATA\\B-Unknown\\*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 0")
+		try:
+			commandInput = 'del /S /Q .\\FR_DATA\\C-Noneface\\*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 1")
+		try:
+			commandInput = 'rd /S /Q .\\FR_DATA\\D-Singleface\\'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 2")
+		try:
+			commandInput = "mkdir .\\FR_DATA\\D-Singleface"
+			commandImplementation = os.popen(commandInput)
+			print("Windows - mkdir...")
+		except Exception as e:
+			raise e
+		try:
+			commandInput = 'del /S /Q .\\FR_DATA\\E-Morefaces\\*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 3")
+		print("Windows - 清除已识别人物数据中...请检查是否清除。")
 
-	try:
-		commandInput = 'rm ./FR_DATA/C-Noneface/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 1")
-
-	try:
-		commandInput = 'rm -r ./FR_DATA/D-Singleface/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 2")
-
-	try:
-		commandInput = 'rm ./FR_DATA/E-Morefaces/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 3")
-	print("\n\033[5;31;40m清除已识别人物数据中...下面如有报错，请忽略。\033[0m\n")
+	else:
+		try:
+			commandInput = 'rm ./FR_DATA/B-Unknown/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 0")
+		try:
+			commandInput = 'rm ./FR_DATA/C-Noneface/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 1")
+		try:
+			commandInput = 'rm -r ./FR_DATA/D-Singleface/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 2")
+		try:
+			commandInput = 'rm ./FR_DATA/E-Morefaces/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 3")
+		print("\n\033[5;31;40m清除已识别人物数据中...下面如有报错，请忽略。\033[0m\n")
 
 if __name__ == "__main__":
 	cleanUpRecognized()
