abcf97f18aa9a8c709905cfae94192b094dc48c0
Ryan <justaaaa@163.com>
2020-07-01 18:14:43 +0800

    update eaCLEAN_UP_TEMP.py.

--- eaCLEAN_UP_TEMP.py
+++ eaCLEAN_UP_TEMP.py
@@ -4,26 +4,49 @@
 '''
 import os
 
+WINDOWS=os.sep=="\\"
+SS=os.sep
+
 #mainX
 def cleanUpTemp():
-	try:
-		commandInput = 'rm ./tempMore/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 0")
+	if WINDOWS:
+		try:
+			commandInput = 'del /S /Q .\\tempMore\\*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 0")
 
-	try:
-		commandInput = 'rm ./tempNone/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 1")
+		try:
+			commandInput = 'del /S /Q .\\tempNone\\*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 1")
 
-	try:
-		commandInput = 'rm ./tempSingle/*'
-		commandImplementation = os.popen(commandInput)
-	except Exception as e:
-		print("ERROR 2")
-	print("\n\033[5;31;40m临时文件已经清除。下面如有报错，请忽略。\033[0m\n")
+		try:
+			commandInput = 'del /S /Q .\\tempSingle\\*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 2")
+		print("Windows - 请检查temp* 内的文件是否清除。")
+	else:
+		try:
+			commandInput = 'rm ./tempMore/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 0")
+
+		try:
+			commandInput = 'rm ./tempNone/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 1")
+
+		try:
+			commandInput = 'rm ./tempSingle/*'
+			commandImplementation = os.popen(commandInput)
+		except Exception as e:
+			print("ERROR 2")
+		print("\n\033[5;31;40m临时文件已经清除。下面如有报错，请忽略。\033[0m\n")
 
 if __name__ == "__main__":
 	cleanUpTemp()
