f6235bc90e0a6c724680b42f565de0ab247f30d5
Ryan <justaaaa@163.com>
2020-07-01 18:11:12 +0800

    update baTrainingOneProcessing.py.

--- baTrainingOneProcessing.py
+++ baTrainingOneProcessing.py
@@ -14,6 +14,12 @@ import face_recognition
 from face_recognition import face_locations
 from face_recognition.face_recognition_cli import image_files_in_folder
 from datetime import datetime
+import os
+import Wait
+
+verbose=False
+WINDOWS=os.sep=="\\"
+SS=os.sep
 
 def __train(train_dir, model_save_path = "", n_neighbors = None, knn_algo = 'ball_tree', verbose=False):
 	TASK = __calcTask(train_dir)
@@ -23,13 +29,15 @@ def __train(train_dir, model_save_path = "", n_neighbors = None, knn_algo = 'bal
 	num = 1
 	for class_dir in listdir(train_dir):
 		n += 1
-		print("\033[1;33;40m添加第{}个训练对象 \033[0m:\033[1;36;40m".format(n) + class_dir + "\033[0m")
+		if verbose:
+			print("\033[1;33;40m添加第{}个训练对象 \033[0m:\033[1;36;40m".format(n) + class_dir + "\033[0m")
 		if not isdir(join(train_dir, class_dir)):
-			print("continue.")
 			continue
 		for img_path in image_files_in_folder(join(train_dir, class_dir)):
 			image = face_recognition.load_image_file(img_path)
-			print("\033[1;34;40m添加第({0}/{1})个文件 \033[0m:\033[1;38;40m".format(num,TASK) + img_path + "\033[0m")
+			if verbose:
+				print("\033[1;34;40m添加第({0}/{1})个文件 \033[0m:\033[1;38;40m".format(num,TASK) + img_path + "\033[0m")
+			Wait.view(num,TASK,"32","")
 			num+=1
 			faces_bboxes = face_locations(image)
 			if len(faces_bboxes) != 1:
@@ -57,7 +65,7 @@ def __calcTask(path):
 	dir = listdir(path)
 	task = 0
 	for person in dir:
-		subDir = path+"/"+person
+		subDir = path+ SS +person
 		num = len(listdir(subDir))
 		task = task + num
 	return task
@@ -67,9 +75,13 @@ def main(train_dir,model_save_path):
 	print("\033[5;33;40m开始训练模型(单线程)....\033[0m\n")
 	time=datetime.now()
 	#./FR_DATA/"{train_dir}/		./KNN_MOD/{name}
-	knn_clf = __train("./FR_DATA/"+train_dir+"/","./KNN_MOD/"+model_save_path+str(time),None,"ball_tree",True)
+	if WINDOWS:
+		TI=str(time).replace(":","")
+		knn_clf = __train(".{0}FR_DATA{1}".format(SS,SS)+train_dir+SS,".{0}KNN_MOD{1}".format(SS,SS)+model_save_path+TI.replace(" ",""),None,"ball_tree",verbose)
+	else:
+		knn_clf = __train(".{0}FR_DATA{1}".format(SS,SS)+train_dir+SS,".{0}KNN_MOD{1}".format(SS,SS)+model_save_path+str(time).replace(" ",""),None,"ball_tree",verbose)
 	print("\n\033[5;31;40m模型训练结束，已经导出到KNN_MOD文件夹下。\033[0m\n")
 
 if __name__ == "__main__":
 	#训练的文件夹/输出模型文件名
-	main("G-WorldWidePeople","WorldWidePeople")
+	main("A-KnownPeople","KnownPeople")
